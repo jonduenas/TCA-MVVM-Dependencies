f55109b76c3560c38eb3d9d72cb41e1c9256ce8f
Jon Duenas <51519363+jonduenas@users.noreply.github.com>
2024-05-06 11:41:55 -0400

    Initial Commit

---
+++ TCA-MVVM-Dependencies/TCA_MVVM_DependenciesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_MVVM_DependenciesApp.swift
+//  TCA-MVVM-Dependencies
+//
+//  Created by Jon Duenas on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TCA_MVVM_DependenciesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
